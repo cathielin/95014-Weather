88267aaf5527557bfb51fd12c1f383f67f2706f5
Cathie Lin <cath.ee.lin@gmail.com>
2020-08-20 13:09:57 -0700

    Update index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-//inside public, we have the basic index.html and we have a div with root
