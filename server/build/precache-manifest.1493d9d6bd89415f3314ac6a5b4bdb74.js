87bc34e10893c329fa7230c8a636d55ac2349a24
Cathie Lin <cath.ee.lin@gmail.com>
2020-08-31 19:02:28 -0700

    Server

---
+++ server/build/precache-manifest.1493d9d6bd89415f3314ac6a5b4bdb74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e256de523a8db9842b35ecc4e0373c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1f354b29ae180327da4",
+    "url": "/static/css/main.3ab8f337.chunk.css"
+  },
+  {
+    "revision": "87e972f96bed56e2ce9c",
+    "url": "/static/js/2.1ecc7281.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.1ecc7281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f354b29ae180327da4",
+    "url": "/static/js/main.7a2ab1de.chunk.js"
+  },
+  {
+    "revision": "3772c0f81f7ab39a145b",
+    "url": "/static/js/runtime-main.06cda749.js"
+  },
+  {
+    "revision": "207e8e3c63144d5a6aa9534bab73ae16",
+    "url": "/static/media/apptitle2.207e8e3c.png"
+  }
+]);
